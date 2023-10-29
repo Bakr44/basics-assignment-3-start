83aa8ae9e9acc9385a8f2d48ca68f0436fbbd4fa
Bakr Hawsawi <bakrhawsawi4@gmail.com>
2023-10-29 17:04:52 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basics-assignment-3-start';
+  showParagraph = false;
+  log: string[] = [];
+  // log: number[] = [];
+  toggleParagraph() {
+    this.showParagraph = !this.showParagraph;
+    // this.log.push(this.log.length + 1);
+    const timestamp = new Date().toLocaleTimeString();
+    this.log.push(this.log.length + 1 + ` Button clicked at ${timestamp}`);
+  }
+
 }
